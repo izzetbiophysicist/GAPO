a06e464b859bc820a57b5e2c7d011678bdd042b1
Lucas Machado <66788893+izzetbiophysicist@users.noreply.github.com>
2023-02-13 08:23:13 -0300

    Add files via upload

---
+++ apt_function.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 19:52:36 2023
+
+@author: lucas
+"""
+
+from pyrosetta import *
+from rosetta.core.pack.task import TaskFactory
+from rosetta.core.pack.task import operation
+
+pyrosetta.init()
+
+
+scorefxn = pyrosetta.create_score_function("ref2015_cart.wts")
+
+######### take sequence, mutate PDB, return score
+
+
+def mutate_repack(pose, posi, amino):
+     #Select position to mutate
+    mut_posi = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector()
+    mut_posi.set_index(posi)
+    
+    #Select neighbor positions
+    nbr_selector = pyrosetta.rosetta.core.select.residue_selector.NeighborhoodResidueSelector()
+    nbr_selector.set_focus_selector(mut_posi)
+    nbr_selector.set_include_focus_in_subset(True)
+    
+    not_design = pyrosetta.rosetta.core.select.residue_selector.NotResidueSelector(mut_posi)
+
+    tf = pyrosetta.rosetta.core.pack.task.TaskFactory()
+
+    tf.push_back(pyrosetta.rosetta.core.pack.task.operation.InitializeFromCommandline())
+    tf.push_back(pyrosetta.rosetta.core.pack.task.operation.IncludeCurrent())
+    tf.push_back(pyrosetta.rosetta.core.pack.task.operation.NoRepackDisulfides())
+
+    # Disable Packing
+    prevent_repacking_rlt = pyrosetta.rosetta.core.pack.task.operation.PreventRepackingRLT()
+    prevent_subset_repacking = pyrosetta.rosetta.core.pack.task.operation.OperateOnResidueSubset(prevent_repacking_rlt, nbr_selector, True )
+    tf.push_back(prevent_subset_repacking)
+
+    # Disable design
+    tf.push_back(pyrosetta.rosetta.core.pack.task.operation.OperateOnResidueSubset(pyrosetta.rosetta.core.pack.task.operation.RestrictToRepackingRLT(),not_design))
+
+    # Enable design
+    aa_to_design = pyrosetta.rosetta.core.pack.task.operation.RestrictAbsentCanonicalAASRLT()
+    aa_to_design.aas_to_keep(amino)
+    tf.push_back(pyrosetta.rosetta.core.pack.task.operation.OperateOnResidueSubset(aa_to_design, mut_posi))
+
+    # Create Packer
+    packer = pyrosetta.rosetta.protocols.minimization_packing.PackRotamersMover()
+    packer.task_factory(tf) 
+    packer.apply(pose)
+    
+    return pose
+
+
+def apt(seq, pose):
+ 
+    ###define starting pose outside of the function
+    
+    for i in range(len(seq)):
+        pose = mutate_repack(pose, posi=i+1, amino=seq[i])
+        score = scorefxn(pose)    
+    ## add pack_relax?
+    return score
+
+
