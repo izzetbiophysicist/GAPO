0fe08108dddf3f44937eba9bb3f1356e4d88e75d
Lucas Machado <66788893+izzetbiophysicist@users.noreply.github.com>
2023-03-15 17:35:57 -0300

    Update GAprot.py

--- GAprot.py
+++ GAprot.py
@@ -11,8 +11,8 @@ import os
 
 os.chdir('/home/lucas/genetic_algo/') 
 
-import genetic_algorithm
-import apt_function
+from genetic_algorithm_rosetta_2 import *
+from apt_function import *
 
 from pyrosetta import *
 from rosetta.core.pack.task import TaskFactory
@@ -27,11 +27,11 @@ pyrosetta.init()
 
 scorefxn = pyrosetta.create_score_function("ref2015_cart.wts")
 starting_pose = pose_from_pdb('/home/lucas/genetic_algo/7lm9a_atom.pdb')
-starting_pose_seq = [x for x in pose.sequence()]
+starting_pose_seq = [x for x in starting_pose.sequence()]
 gene_values = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
 
 
-pop_size = 10
+pop_size = 4
 vector_size = len(starting_pose_seq)
 
 init_pop = [list(starting_pose_seq)]
@@ -40,10 +40,12 @@ new_indiv = [[gene_values[int(np.round(uniform(low=0, high=len(gene_values)-1)))
 init_pop = list(init_pop) + list(new_indiv)
 
 
-GA = genetic_algo(opt_direction='down', gene_values=gene_values, gene_type='discrete', 
-             vector_size=len(starting_pose_seq), pop_size=5, mutation_rate=0.4, segment_fluctuation=0, 
-             apt_function=apt, selection_method='tournament', 
-             convergence_threshold=0, n_cycles=10, n_survivors=5, tournament_size=20, 
-             initial_population=initial_population)
+### If using threads, use apt_threads, if not use APT
+
+GA = genetic_algo(pose=starting_pose, opt_direction='down', gene_values=gene_values, gene_type='discrete', 
+             vector_size=len(starting_pose_seq), pop_size=pop_size+1, mutation_rate=0.4, segment_fluctuation=0, 
+             apt_function=apt, selection_method='tournament', threads=False,
+             convergence_threshold=0, n_cycles=5, n_survivors=1, tournament_size=2,
+             initial_population=init_pop)
 
 GA.execute()
