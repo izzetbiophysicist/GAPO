65b5b53e7a83e232b84e0b80c124d33a110c1efb
Lucas Machado <66788893+izzetbiophysicist@users.noreply.github.com>
2023-03-27 11:02:32 -0300

    Update GAprot.py

--- GAprot.py
+++ GAprot.py
@@ -11,7 +11,7 @@ import os
 
 os.chdir('/home/lucas/genetic_algo/') 
 
-from genetic_algorithm_rosetta_2 import *
+from genetic_algorithm_rosetta import *
 from apt_function import *
 
 from pyrosetta import *
@@ -26,12 +26,12 @@ pyrosetta.init()
 
 
 scorefxn = pyrosetta.create_score_function("ref2015_cart.wts")
-starting_pose = pose_from_pdb('/home/lucas/genetic_algo/7lm9a_atom.pdb')
+starting_pose = pose_from_pdb('/home/lucas/genetic_algo/1lzt.pdb')
 starting_pose_seq = [x for x in starting_pose.sequence()]
 gene_values = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
 
 
-pop_size = 4
+pop_size = 9
 vector_size = len(starting_pose_seq)
 
 init_pop = [list(starting_pose_seq)]
@@ -42,10 +42,25 @@ init_pop = list(init_pop) + list(new_indiv)
 
 ### If using threads, use apt_threads, if not use APT
 
+
+
+
+
 GA = genetic_algo(pose=starting_pose, opt_direction='down', gene_values=gene_values, gene_type='discrete', 
-             vector_size=len(starting_pose_seq), pop_size=pop_size+1, mutation_rate=0.4, segment_fluctuation=0, 
+             vector_size=len(starting_pose_seq), pop_size=len(init_pop), mutation_rate=0.2, segment_fluctuation=0, 
              apt_function=apt, selection_method='tournament', threads=False,
-             convergence_threshold=0, n_cycles=5, n_survivors=1, tournament_size=2,
+             convergence_threshold=0, n_cycles=20, n_survivors=1, tournament_size=4,
+             initial_population=init_pop)
+
+GA.execute()
+
+
+#### Solve threads!
+
+GA = genetic_algo(pose=starting_pose, opt_direction='down', gene_values=gene_values, gene_type='discrete', 
+             vector_size=len(starting_pose_seq), pop_size=len(init_pop), mutation_rate=0.2, segment_fluctuation=0, 
+             apt_function=apt_thread, selection_method='tournament', threads=True,
+             convergence_threshold=0, n_cycles=5, n_survivors=2, tournament_size=4,
              initial_population=init_pop)
 
 GA.execute()
