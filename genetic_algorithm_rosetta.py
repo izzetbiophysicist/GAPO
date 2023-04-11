81ec326b0c26ef8bafe386ced7b6003377548e92
Lucas Machado <66788893+izzetbiophysicist@users.noreply.github.com>
2023-04-11 18:30:29 -0300

    Update genetic_algorithm_rosetta.py

--- genetic_algorithm_rosetta.py
+++ genetic_algorithm_rosetta.py
@@ -16,6 +16,7 @@ from pyrosetta import *
 from rosetta.core.pack.task import TaskFactory
 from rosetta.core.pack.task import operation
 from datetime import datetime
+import pandas as pd
 
 pyrosetta.init()
 
@@ -39,7 +40,7 @@ class thread_rosetta:
 
 
 class genetic_algo:
-    def __init__(self, pose, opt_direction, gene_values, gene_type, vector_size, threads, pop_size, mutation_rate, segment_fluctuation, apt_function, selection_method, convergence_threshold, n_cycles, n_survivors, tournament_size=0, initial_population=[]):
+    def __init__(self, pose, opt_direction, gene_values, gene_type, vector_size, threads, pop_size, mutation_rate, segment_fluctuation, apt_function, selection_method, convergence_threshold, n_cycles, benchmark, crossing_over_type, tournament_size=0, initial_population=[]):
         self.initial_population=initial_population 
         self.population = initial_population
         self.gene_values = gene_values  ### if gene_type = 'continuous' then gene_values should contain the upper and lower bounds
@@ -52,12 +53,13 @@ class genetic_algo:
         self.convergence_threshold = convergence_threshold
         self.n_cycles = n_cycles
         self.gene_type = gene_type
-        self.n_survivors = n_survivors
         self.opt_direction = opt_direction
         self.vector_size = vector_size
         self.pose = pose
         self.scorefxn = pyrosetta.create_score_function("ref2015_cart.wts")
         self.threads = threads
+        self.benchmark = benchmark
+        self.crossing_over_type = crossing_over_type
         
         
         ### recalculate pop_size if the initial population is not randomly generated
@@ -82,36 +84,51 @@ class genetic_algo:
             
     def calculate_scores(self, population, pre_calc=[]):
         
-        if len(pre_calc) == 0 and self.threads==False:
-            print('CALCULATING SCORES!')
-
-        #### Iterate over population and fill self.scores
-            scores = [self.apt_function(population[x], self.pose, self.scorefxn) for x in range(len(population))]
+        if len(pre_calc) == 0:
         
-        ## TEST APT-FUNCTION WHEN NOT USING THREADS
-        
-        if len(pre_calc) == 0 and self.threads==True:
-            print('CALCULATING SCORES!')
-            t1 = thread_rosetta(population, self.pose, self.scorefxn, self.apt_function)
-            t1.run()
+            if self.benchmark==True:
+                print('CALCULATING SCORES!')
+    
+            #### Iterate over population and fill self.scores
+                scores = [self.apt_function(population[x]) for x in range(len(population))]
             
-            scores = [t1.return_results[x][0] for x in range(len(t1.return_results))]
+            else:
+                if self.threads==False:
+                    print('CALCULATING SCORES!')
+        
+                #### Iterate over population and fill self.scores
+                    scores = [self.apt_function(population[x], self.pose, self.scorefxn) for x in range(len(population))]
+                
+                ## TEST APT-FUNCTION WHEN NOT USING THREADS
+                
+                if self.threads==True:
+                    print('CALCULATING SCORES!')
+                    t1 = thread_rosetta(population, self.pose, self.scorefxn, self.apt_function)
+                    t1.run()
+                    
+                    scores = [t1.return_results[x][0] for x in range(len(t1.return_results))]
 
             
+        if len(pre_calc) != 0:
             
-            
-        else:
-            if self.threads == False:
+            if self.benchmark == True:
                 scores = list(pre_calc) 
-                scores_to_append = [self.apt_function(population[x], self.pose, self.scorefxn) for x in range(len(population)) if x >= len(pre_calc)]
+                scores_to_append = [self.apt_function(population[x]) for x in range(len(population)) if x >= len(pre_calc)]
                 scores = scores + scores_to_append
                 
-            if self.threads == True:
-                scores = list(pre_calc) 
-                t1 = thread_rosetta([self.population[x] for x in range(len(population)) if x >= len(pre_calc)], self.pose, self.scorefxn, self.apt_function)
-                t1.run()
-                
-                scores_to_append = [t1.return_results[x][0] for x in range(len(t1.return_results))]
+            else:
+            
+                if self.threads == False:
+                    scores = list(pre_calc) 
+                    scores_to_append = [self.apt_function(population[x], self.pose, self.scorefxn) for x in range(len(population)) if x >= len(pre_calc)]
+                    scores = scores + scores_to_append
+                    
+                if self.threads == True:
+                    scores = list(pre_calc) 
+                    t1 = thread_rosetta([self.population[x] for x in range(len(population)) if x >= len(pre_calc)], self.pose, self.scorefxn, self.apt_function)
+                    t1.run()
+                    
+                    scores_to_append = [t1.return_results[x][0] for x in range(len(t1.return_results))]
 
                 
                 scores = scores + scores_to_append
@@ -119,104 +136,34 @@ class genetic_algo:
                 
         return scores
     
-   
-    def select(self, population, scores):
-        
-        ##### Tournament rounds take N individuals and select them. When N > the remaining population (not subjected to selection yet)
-        ##### the remaining individuals follow to the next step without selection
-        
-            
-        
-        
-        selected_ind = []
-        selected_scores = []
-        
-        dead_ind = []
-        dead_scores = []
-        
-        if self.selection_method == 'tournament':
-            
-            tmp_pop = list(population)
-            tmp_scores = list(scores)
-            
-            while len(tmp_pop) > self.tournament_size:
-                to_select = sample(range(len(tmp_pop)), self.tournament_size)
-                
-                tournament_scores = [tmp_scores[x] for x in to_select]
-                tournament_pop = [tmp_pop[x] for x in to_select]
-                
-                for rounds in range(self.n_survivors):
-                    
-                    if self.opt_direction == 'up':
-                        selected_ind = selected_ind + [tournament_pop[np.argmax(tournament_scores)]]
-                        selected_scores.append(tournament_scores[np.argmax(tournament_scores)]) ## append here
-                    if self.opt_direction == 'down':
-                        selected_ind = selected_ind + [tournament_pop[np.argmin(tournament_scores)]]
-                        selected_scores.append(tournament_scores[np.argmin(tournament_scores)]) ## append here
-                        
-                    if self.opt_direction == 'up':
-                        tournament_pop.pop(np.argmax(tournament_scores))                    
-                        tournament_scores.pop(np.argmax(tournament_scores))
-                    
-                    if self.opt_direction == 'down':
-                        tournament_pop.pop(np.argmin(tournament_scores))                    
-                        tournament_scores.pop(np.argmin(tournament_scores))
-                    
-                    
-                dead_ind = dead_ind + tournament_pop
-                dead_scores = dead_scores + tournament_scores
-                
-                
-                tmp_scores = [tmp_scores[x] for x in range(len(tmp_scores)) if x not in to_select]
-                tmp_pop = [tmp_pop[x] for x in range(len(tmp_pop)) if x not in to_select]
-            
-            return selected_ind, selected_scores, dead_ind, dead_scores
-            
-        if self.selection_method == 'elitist':
-            selected_ind = []
-            selected_scores = []
-            
-            dead_ind = []
-            dead_scores = []
-            
-            tmp_pop = list(population)
-            tmp_scores = list(scores)
-            
-            while len(tmp_pop) != self.n_survivors:
-                
-                
-                if self.opt_direction == 'up':
-
-                    selected_ind = selected_ind + [tmp_pop[np.argmax(tmp_scores)]]
-                    selected_scores.append(tmp_scores[np.argmax(tmp_scores)]) ## append here
-                    
-                    tmp_pop.pop(np.argmax(tmp_scores))                    
-                    tmp_scores.pop(np.argmax(tmp_scores))
-                
-                if self.opt_direction == 'down':
-
-                    selected_ind = selected_ind + [tmp_pop[np.argmin(tmp_scores)]]
-                    selected_scores.append(tmp_scores[np.argmin(tmp_scores)]) ## append here
-                    
-                    tmp_pop.pop(np.argmin(tmp_scores))                    
-                    tmp_scores.pop(np.argmin(tmp_scores))
-                    
-                dead_ind = list(tmp_pop)
-                dead_scores = list(tmp_scores)
-
-            
-            return selected_ind, selected_scores, dead_ind, dead_scores
-        
     
-    def crossing_over(self, ind1, ind2):
-        if self.segment_fluctuation+(len(ind1)/2) > len(ind1):
-            print('segment fluctuation is too long') 
-            
-        #sample(segment_fluctuation, 1)
-        fluct=int(np.round(uniform(low=0, high=self.segment_fluctuation)))
-        newind1 = ind1[0:int((len(ind1)/2)+fluct)] + ind2[(int((len(ind2)/2)+fluct)):len(ind2)+1]
-        newind2 = ind2[0:int((len(ind2)/2)+fluct)] + ind1[(int((len(ind1)/2)+fluct)):len(ind1)+1]        
+    def crossing_over(self, ind1, ind2, crossing_over_type):
         
+        if crossing_over_type == 'punctual':
+            if self.segment_fluctuation+(len(ind1)/2) > len(ind1):
+                print('segment fluctuation is too long') 
+                
+            #sample(segment_fluctuation, 1)
+            fluct=int(np.round(uniform(low=0, high=self.segment_fluctuation)))
+            newind1 = ind1[0:int((len(ind1)/2)+fluct)] + ind2[(int((len(ind2)/2)+fluct)):len(ind2)+1]
+            newind2 = ind2[0:int((len(ind2)/2)+fluct)] + ind1[(int((len(ind1)/2)+fluct)):len(ind1)+1]        
+        
+        if crossing_over_type == 'mask':
+            
+            newind1 = ind1
+            newind2 = ind2
+            
+            mask = [int(np.round(uniform(low=0, high=1))) for x in range(self.vector_size)]
+            #positive = [x for x in range(len(mask)) if mask[x] == 1]
+            #negative = [x for x in range(len(mask)) if mask[x] != 1]
+            
+            newind1 = ind1
+            newind1 = [ind1[x] if mask[x] == 1 else ind2[x] for x in range(len(ind2))]
+            
+            newind2 = ind2
+            newind1 = [ind1[x] if mask[x] != 1 else ind2[x] for x in range(len(ind2))]
+            
+            
         return newind1, newind2
     
     def mutate(self, ind):
@@ -234,33 +181,45 @@ class genetic_algo:
             
         return ind
         
+    def breed(self, population, scores):
+        if self.selection_method == 'tournament':
+            
+            # Create pandas dict to facilitate sorting
+            pop_scores = {'indv': population , 'score': scores}
+            init_pop = pd.DataFrame(pop_scores)
+            init_pop = init_pop.sort_values('score', ascending=False)
+        
+            
+            ### Initiate offspring dataframe
+            #offspring = {'indv': [] , 'score': []}
+            #offspring = pd.DataFrame(offspring)
+            offspring = []
+            
+            #### number of tournament rounds are population/2 
+            for rounds in range(int(len(population)/2)):
+                to_select = init_pop.iloc[sample(range(len(population)), 2)]               
+                to_select = to_select.sort_values('score', ascending=False)
     
-    
-    def repopulate(self, selected, selected_scores, dead, dead_scores):
-        
-        
-        new_population = selected
-        
-        
-        while len(new_population) != self.pop_size:
+                co = self.crossing_over(to_select.iloc[0][0], to_select.iloc[1][0], self.crossing_over_type)
+                for newindv in co:
+                    offspring.append(newindv)
+                    
             
-            ind1=selected[int(np.round(uniform(low=[0], high=(len(selected)-1))))]
-            ind2=dead[int(np.round(uniform(low=[0], high=(len(dead)-1))))]
-            
-            offspring = self.crossing_over(ind1, ind2)
-            
-            i=0
-            for i in range(2):
-                if len(new_population) != self.pop_size:
-                    new_population.append(list(offspring[i]))
-                    if uniform(low=0, high=1) < self.mutation_rate:
-                        new_population[len(new_population)-1] = self.mutate(new_population[len(new_population)-1])
-                
-                i=+1
+            for trymut in range(len(offspring)):
+                if uniform(low=0, high=1) < self.mutation_rate:
+                    offspring[trymut] = self.mutate(offspring[trymut])
+                        
+            offspring_scores = self.calculate_scores(offspring)
             
             
-        return new_population
-        
+            
+            
+            whole_pop = {'indv': population+offspring , 'score': scores+offspring_scores}
+            whole_pop = pd.DataFrame(whole_pop)
+            whole_pop = whole_pop.sort_values('score', ascending=False)
+            whole_pop = whole_pop[0:self.pop_size]
+
+            return whole_pop['indv'].to_list(), whole_pop['score'].to_list()
             
                 
     def opt_cycle(self):
@@ -282,14 +241,14 @@ class genetic_algo:
         for t in range(self.n_cycles):
             print('Running round '+str(t))
             
-            select_round = self.select(self.population, self.scores)
-
-            self.population = self.repopulate(selected=select_round[0], selected_scores=select_round[1], dead=select_round[2], dead_scores=select_round[3])
-            self.pop_history.append(self.population)
-
-            self.scores = self.calculate_scores(self.population, pre_calc=select_round[1])
-
+            new_pop = self.breed(self.population, self.scores)
             
+            self.population = new_pop[0]
+            self.scores = new_pop[1]
+
+
+            #### Tracking variables            
+            self.pop_history.append(self.population)
             self.t=t
             print(t)
             self.best.append(self.scores[np.argmax(self.scores)])
